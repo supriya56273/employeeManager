9ed9ee339ed29232637815b9bb85f50117478717
kishansinghchauhan <kishancoer7071@gmail.com>
2020-04-02 21:43:21 +0530

    Initial commit

---
+++ script.sh
@@ -0,0 +1,11 @@
+# Install Node, npm & then
+npm install -g @angular/cli # for installing Angular
+ng new employeeManager # for creating new Angular project
+cd employeeManager # Tatkes you to project Directory
+npm i # for installing all dependencies
+ng version # for getting the angular, node and cli versions
+ng serve --open # for running the realtime envoirnment at port number 4200
+npm i --save @ng-bootstrap/ng-bootstrap bootstrap font-awesome # for installing bootstrap and font-awesome
+ng g c employees # for generating new employee component
+ng g c employee # for generating new employees component
+ng g c employee-list # for generating new employee-list component
---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { EmployeeComponent } from './employees/employee/employee.component';
+import { EmployeeListComponent } from './employees/employee-list/employee-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmployeesComponent,
+    EmployeeComponent,
+    EmployeeListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
